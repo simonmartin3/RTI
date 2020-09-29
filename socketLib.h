e1019edf06e378059459b9f38536c9a967059139
Simon MARTIN <simon.martin3@student.hepl.be>
2020-09-29 11:48:33 +0200

    Modification lib

--- socketLib.h
+++ socketLib.h
@@ -4,11 +4,29 @@
 // MARTIN Simon
 //---------------------------------
 
+#ifndef SOCKET_H
+#define SOCKET_H
 
-#ifndef LIBRARYSERVER
-#define LIBRARYSERVER
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
-void setCMMP(int type , const char* msg);
-CMMP getCMMP();
+#include <unistd.h>
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <netdb.h>
+#include <errno.h>
+#include <netinet/in.h>
+#include <netinet/tcp.h>
+#include <arpa/inet.h>
+#include <time.h>
+#include <pthread.h>
+
+#define PORT 5000
+
+
+int SocketInit(struct sockaddr_in* , char* , unsigned long );
+int SocketInitWithIpAddr(struct sockaddr_in*, char*, unsigned long, char* );
+void ListenAccept(int *, int *, struct sockaddr *);
 
 #endif
