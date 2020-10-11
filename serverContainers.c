499e3f2ea2500ca58f608558608ab9461d53447c
Simon MARTIN <simon.martin3@student.hepl.be>
2020-10-11 23:01:40 +0200

    Modified serverContainers

--- serverContainers.c
+++ serverContainers.c
@@ -83,13 +83,13 @@ int main ()
 
 /* Ouverture du fichier de configuration */
 
-    port = atoi(searchConfig("PORT_SERVEUR"));
-    fileLog = searchConfig("FILELOG");
+    fileLog = searchConfig("PORT_SERVEUR");
+    //fileLog = searchConfig("FILELOG");
     fileParc = searchConfig("FILEPARC");
     // sepCsv = searchConfig("SEP_CSV");
 
-    printf("%d\n", port);
-    //printf("%s\n", fileLog);
+    //printf("%d\n", port);
+    printf("%s\n", fileLog);
     //printf("%s\n", fileParc);
 
 /* Ouverture et/ou création du fichier login.csv & FICH_PARC */
