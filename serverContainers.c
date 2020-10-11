64f90b6ac1e0453fc33a737467795a9abeb63d4c
Simon MARTIN <simon.martin3@student.hepl.be>
2020-10-11 23:25:37 +0200

    Modified serverContainers

--- serverContainers.c
+++ serverContainers.c
@@ -115,7 +115,7 @@ int main ()
     for (i=0; i<NB_MAX_CLIENTS; i++) 
 		hSocketConnectee[i] = -1;
 
-	hSocketEcoute = SocketInit(&adresseSocket, "solaris11DM2017", port);
+	hSocketEcoute = SocketInit(&adresseSocket, "solaris11DM2017", PORT);
 
 /* 4. Le système prend connaissance de l'adresse et du port de la socket */
 	if (bind(hSocketEcoute, (struct sockaddr *)&adresseSocket, sizeof(struct sockaddr_in)) == -1)
