acde94cab2b81ccebd41527c1cb683c60c04cd08
Simon MARTIN <simon.martin3@student.hepl.be>
2020-10-14 18:54:40 +0200

    Modified serverContainer

--- utilLib.c
+++ utilLib.c
@@ -357,7 +357,7 @@ void pressEnter()
             }
         }
 
-        return listContainer;
+        return *listContainer;
     }
 
 /*----------------------------------------------------------------*/
