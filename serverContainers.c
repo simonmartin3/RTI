83f5dee8915ce8ba91528a4a47f820ffd2beafe9
Simon MARTIN <simon.martin3@student.hepl.be>
2020-10-14 19:48:54 +0200

    Modified serverContainer

--- serverContainers.c
+++ serverContainers.c
@@ -225,7 +225,7 @@ int main ()
                         break;
 
 		            case 3:
-		                memcpy(&listSend, outputVehicule(msgRecv.msg, FILEPARC), 50*sizeof(Container));
+		                listSend = outputVehicule(msgRecv.msg, FILEPARC);
                         printf("%s\n", listSend[0].idContainer);
                         // if (send(hSocketServ, listSend, sizeof(listSend), 0) == -1)
                         // {
